071ef303fd84a7cded7f193bb3d74a5a6dda72a8
fanpero87 <FAb1hn0!>
2021-09-26 12:00:29 -0400

    working app

---
+++ js/todos.js
@@ -0,0 +1,97 @@
+window.todoStore = {
+	todos: JSON.parse(localStorage.getItem("todo-store") || "[]"),
+
+	save() {
+		localStorage.setItem("todo-store", JSON.stringify(this.todos));
+	},
+};
+
+window.Todo = function (body) {
+	this.id = Date.now();
+	this.body = body;
+	this.completed = false;
+};
+
+window.todos = function () {
+	return {
+		...todoStore,
+		filter: "all",
+		newTodo: "",
+		editedTodo: null,
+
+		get active() {
+			return this.todos.filter((todo) => !todo.completed);
+		},
+
+		get completed() {
+			return this.todos.filter((todo) => todo.completed);
+		},
+
+		get filteredTodos() {
+			return {
+				all: this.todos,
+				active: this.active,
+				completed: this.completed,
+			}[this.filter];
+		},
+
+		get allComplete() {
+			return this.todos.length === this.completed.length;
+		},
+
+		addTodo() {
+			if (!this.newTodo) return;
+			this.todos.push(new Todo(this.newTodo));
+			this.save();
+			this.newTodo = "";
+		},
+
+		editTodo(todo) {
+			todo.cachedBody = todo.body;
+			this.editedTodo = todo;
+		},
+
+		cancelEdit(todo) {
+			todo.body = todo.cachedBody;
+			this.editedTodo = null;
+			delete todo.cachedBody;
+		},
+
+		editComplete(todo) {
+			if (todo.body.trim() === "") {
+				return this.deleteTodo(todo);
+			}
+			this.editedTodo = null;
+			this.save();
+		},
+
+		completeTodo(todo) {
+			todo.completed = true;
+		},
+
+		deleteTodo(todo) {
+			let position = this.todos.indexOf(todo);
+			this.todos.splice(position, 1);
+
+			this.save();
+		},
+
+		toggleTodoCompletion(todo) {
+			todo.completed = !todo.completed;
+
+			this.save();
+		},
+
+		toggleAllComplete() {
+			let allComplete = this.allComplete;
+			this.todos.forEach((todo) => (todo.completed = !allComplete));
+			this.save();
+		},
+
+		clearCompletedTodos() {
+			this.todos = this.active;
+
+			this.save();
+		},
+	};
+};
